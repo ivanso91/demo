c7091f6b83be0cb20dcf8073d65ce53aad479469
Ruben Svansåsbakken Sevaldson <r.sevaldson@gmail.com>
2018-02-06 16:31:33 +0100

    HAHAHAHAHAHAHA

--- main.c
+++ main.c
@@ -4,7 +4,10 @@ int main(){
 	printf("Hello, world\n");
 	printf("...and Mars\n");
 	printf("...and Jupiter\n");
-	
+	while (1)
+	{
+		printf("ikke noe linjeskift?????!!!! \n\n\n");
+	}
 	if (1 > 0){
 		return 1;
 	}
